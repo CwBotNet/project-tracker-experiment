15f76ba3e04f3de9341d0d42e0116802db74329b
Raj sahani <Rajsahaniofficial19@gmail.com>
2024-05-28 14:07:52 +0530

    kv storege bug fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
 import { Hono } from "hono";
-import { healthCheckHandler, xSetupHandler } from "./middleware";
+import { cors } from "hono/cors";
+import {
+  healthCheckHandler,
+  xCallabckHandler,
+  xSetupHandler,
+} from "./middleware";
+import { KVNamespace } from "@cloudflare/workers-types";
+type Bindings = {
+  TWITTER_API_BASE: string;
+  CLIENT_ID: string;
+  CLIENT_SECRET: string;
+  REDIRECT_URI: string;
+  NOTION_TOKEN: string;
+  NOTION_DATABASE_ID: string;
+  STATE_KV: KVNamespace;
+  CODE_VERIFIER_KV: KVNamespace;
+  // [key: string]: KVNamespace | string;
+};
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Bindings }>();
+app.use("/*", cors());
 
 /*-------------------------routes-----------------------*/
 
@@ -11,6 +29,41 @@ app.get("/", ...healthCheckHandler);
 app.get("/oAuth2", ...xSetupHandler);
 
 /*-------------------------x-callback-route-----------------------*/
-app.get("/callback", async (c) => {});
+app.get("/callback", ...xCallabckHandler);
+
+app.post("/kv-update-post", async (c) => {
+  try {
+    await c.env.STATE_KV.put("state", "stateUPdated");
+    await c.env.CODE_VERIFIER_KV.put("verifier", "codeVerifierUPdated");
+
+    const state = await c.env.STATE_KV.get("state");
+    const codeVerifier = await c.env.CODE_VERIFIER_KV.get("verifier");
+    return c.json({
+      success: true,
+      state: state,
+      codeVerifier: codeVerifier,
+    });
+  } catch (error: any) {
+    console.log(error?.message);
+    return c.text("Error", error.message);
+  }
+});
+app.get("/kv-update-get", async (c) => {
+  try {
+    await c.env.STATE_KV.put("state", "stateUPdated-get");
+    await c.env.CODE_VERIFIER_KV.put("verifier", "codeVerifierUPdated-get");
+
+    const state = await c.env.STATE_KV.get("state");
+    const codeVerifier = await c.env.CODE_VERIFIER_KV.get("verifier");
+    return c.json({
+      success: true,
+      state: state,
+      codeVerifier: codeVerifier,
+    });
+  } catch (error: any) {
+    console.log(error?.message);
+    return c.text("Error", error.message);
+  }
+});
 
 export default app;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { createFactory } from "hono/factory";
 import { generateCodeChallenge, generateCodeVerifier } from "./helper/utils";
-import { generateOAuth2AuthLink } from "./twitter";
+import { generateOAuth2AuthLink, loginWithOauth2 } from "./twitter";
 import { storeTokenInNotion } from "./notion";
 
 const factory = createFactory();
@@ -11,7 +11,7 @@ const xSetupHandler = factory.createHandlers(async (c) => {
     const state: string = crypto.randomUUID();
     const challenge = await generateCodeChallenge(verifier);
 
-    const { REDIRECT_URI, CLIENT_ID, codeVerifire_kv, state_kv } = c.env;
+    const { REDIRECT_URI, CLIENT_ID, CODE_VERIFIER_KV, STATE_KV } = c.env;
     const { url } = await generateOAuth2AuthLink({
       callbackUrl: REDIRECT_URI,
       state: state,
@@ -20,23 +20,16 @@ const xSetupHandler = factory.createHandlers(async (c) => {
       scope: "tweet.read users.read follows.read follows.write",
       clientId: CLIENT_ID,
     });
-
     // Save verifier and state in KV storage
-    console.log("storing kv values");
-    let resCode = await codeVerifire_kv.put("verifier", `${verifier}`);
-    let resState = await state_kv.put("state", `${state}`);
-    if (!resCode || !resState) {
-      console.log({
-        resCode,
-        resState,
-      });
-      console.log("failed to store kv values");
-    } else {
-      console.log("stored kv values");
-    }
+    console.log("Storing KV values");
+    await c.env.CODE_VERIFIER_KV.put("verifier", verifier);
+    await c.env.STATE_KV.put("state", state);
+
+    console.log("KV values stored successfully");
+
     // Retrieve and log stored values to verify
-    const storedVerifier = await codeVerifire_kv.get("verifier");
-    const storedState = await state_kv.get("state");
+    const storedVerifier = await c.env.CODE_VERIFIER_KV.get("verifier");
+    const storedState = await c.env.STATE_KV.get("state");
     console.log({
       url: url,
       codeVerifier: storedVerifier,
@@ -53,16 +46,9 @@ const xSetupHandler = factory.createHandlers(async (c) => {
 const xCallabckHandler = factory.createHandlers(async (c) => {
   try {
     const { code, state } = c.req.query();
-    const {
-      TWITTER_API_BASE,
-      CLIENT_ID,
-      CLIENT_SECRET,
-      REDIRECT_URI,
-      state_kv,
-      codeVerifire_kv,
-    } = c.env;
+    const { TWITTER_API_BASE, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = c.env;
 
-    const storedState = await state_kv.get("state");
+    const storedState = await c.env.STATE_KV.get("state");
     console.log("Stored state:", storedState);
 
     if (!storedState || storedState !== state) {
@@ -71,35 +57,61 @@ const xCallabckHandler = factory.createHandlers(async (c) => {
       return c.json({ error: "Invalid or expired state" }, 400);
     }
 
-    const storedCodeVerifire = await codeVerifire_kv.get("verifier");
+    const storedCodeVerifire = await c.env.CODE_VERIFIER_KV.get("verifier");
     console.log("Stored code verifier:", storedCodeVerifire);
 
     if (!storedCodeVerifire) {
       return c.json({ error: "Invalid or expired verifier" }, 400);
     }
 
-    const tokenResponse = await fetch(`${TWITTER_API_BASE}/2/oauth2/token`, {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams({
-        code,
-        grant_type: "authorization_code",
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        redirect_uri: REDIRECT_URI,
-        code_verifier: storedCodeVerifire,
-      }),
+    // const tokenResponse = await fetch(`${TWITTER_API_BASE}/2/oauth2/token`, {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //   body: new URLSearchParams({
+    //     code,
+    //     grant_type: "authorization_code",
+    //     client_id: CLIENT_ID,
+    //     redirect_uri: REDIRECT_URI,
+    //     code_verifier: storedCodeVerifire,
+    //     client_secret: CLIENT_SECRET,
+    //   }),
+    // });
+
+    const tokenResponse = await loginWithOauth2({
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      code: code,
+      codeVerifier: storedCodeVerifire,
+      redirectUri: REDIRECT_URI,
     });
 
-    if (!tokenResponse.ok) {
+    console.log(await tokenResponse);
+
+    if (!tokenResponse.accessToken) {
       return c.json({ error: "Failed to fetch token" }, 500);
     }
 
-    const tokenData: any = await tokenResponse.json();
-    await storeTokenInNotion(tokenData);
+    // const { NOTION_DATABASE_ID, NOTION_TOKEN } = c.env;
+    // const currentDate = Date.now();
+    // await storeTokenInNotion({
+    //   AccessToken: tokenResponse.accessToken,
+    //   refreshToken: tokenResponse.refreshToken,
+    //   Name: "xClinet",
+    //   date: currentDate.toString(),
+    //   notion: {
+    //     apiKey: NOTION_TOKEN,
+    //     databaseId: NOTION_DATABASE_ID,
+    //   },
+    // });
 
-    console.log("Callback route executed successfully");
-    return c.json(tokenData);
+    console.log("Callback route executed successfully", tokenResponse);
+    return c.json(
+      {
+        accessToken: tokenResponse.accessToken,
+        refreshtoken: tokenResponse.refreshToken,
+      },
+      200
+    );
   } catch (error) {
     console.error("Error during callback handler execution:", error);
     return new Response("Internal Server Error", { status: 500 });
